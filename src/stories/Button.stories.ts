d8c11258b884fe3185cb0654ea2b2dc247327c93
NajeebSultan <najeebsultan070@gmail.com>
2025-04-29 19:33:21 +0500

    Add Button component stories to Storybook

---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,10 @@
+import {Meta} from '@storybook/react';
+import {Button} from '@/components/Button';
+import {ComponentProps} from 'react';
+
+
+type StoryProps = ComponentProps<typeof Button> ;
+
+const meta : Meta<StoryProps> = {};
+
+export default meta;
