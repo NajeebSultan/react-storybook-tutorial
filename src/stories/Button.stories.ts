45cbc64b4d9ca5ea831e3eb19e9c991df2cacdeb
NajeebSultan <najeebsultan070@gmail.com>
2025-04-29 19:58:07 +0500

    Enhance Button stories by adding component metadata and improving structure

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,10 +1,23 @@
-import {Meta} from '@storybook/react';
+import {Meta , StoryObj} from '@storybook/react';
 import {Button} from '@/components/Button';
 import {ComponentProps} from 'react';
 
 
 type StoryProps = ComponentProps<typeof Button> ;
 
-const meta : Meta<StoryProps> = {};
+const meta : Meta<StoryProps> = {
+  component: Button,
+};
 
 export default meta;
+
+type Story = StoryObj<StoryProps>
+export const Primary: Story = {
+  args:
+  {
+    variant: 'primary',
+    size: 'md',
+    
+
+  }
+}
